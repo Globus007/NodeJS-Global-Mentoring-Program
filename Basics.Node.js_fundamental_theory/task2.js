0e071dad0999c50c1f9582e91d4fd94bfd82a9a7
gleb_stashinsky <gleb_stashinsky@epam.com>
2021-09-24 17:22:05 +0300

    feat: implement task2

---
+++ Basics.Node.js_fundamental_theory/task2.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const { pipeline } = require('stream')
+const csv = require('csvtojson');
+const fs = require("fs");
+
+const INPUT_FILENAME = 'csv/nodejs-hw1-ex1.csv';
+const OUTPUT_FILENAME = 'csv/output.txt';
+
+const inputPath = path.join(__dirname, INPUT_FILENAME);
+const outputPath = path.join(__dirname, OUTPUT_FILENAME);
+
+pipeline(
+    csv({delimiter: ';'}).fromFile(inputPath).subscribe((json) => json),
+    fs.createWriteStream(outputPath),
+    (err) => {
+       if (err) {
+           console.error('Processing file failed.', err);
+       }
+    }
+);
