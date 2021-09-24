e62d616b584f7f59ce738a5fa723f0e6549e0465
gleb_stashinsky <gleb_stashinsky@epam.com>
2021-09-24 16:13:50 +0300

    feat: implement task1

---
+++ task1.js
@@ -0,0 +1,16 @@
+const { stdin, stdout } = require('process');
+const stream = require("stream");
+
+class ReverseStream extends stream.Transform {
+    _transform(chunk, encoding, callback) {
+        const str = chunk.toString().replace('\n', '');
+        const reverseStr = str.split('').reverse().join('');
+
+        this.push(`${reverseStr}\n\n`);
+        callback();
+    }
+}
+
+const transformStream = new ReverseStream();
+
+stdin.pipe(transformStream).pipe(stdout);
