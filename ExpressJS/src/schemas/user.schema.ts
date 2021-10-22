c7ac5e8711b03430980523a8a1b2a151ed2a97f6
Gleb Stashinsky <g.stashinsky@gmail.com>
2021-10-22 13:30:33 +0300

    fix: fix password validation

--- ExpressJS/src/schemas/user.schema.ts
+++ ExpressJS/src/schemas/user.schema.ts
@@ -5,7 +5,10 @@ export const UserSchema = Joi.object({
 
   login: Joi.string().required(),
 
-  password: Joi.string().alphanum().required(),
+  password: Joi.string()
+    .regex(/\d/, '"must contain at least one number"')
+    .regex(/[A-Za-z]/, '"must contain at least one letter"')
+    .required(),
 
   age: Joi.number().integer().min(4).max(130).required(),
 
