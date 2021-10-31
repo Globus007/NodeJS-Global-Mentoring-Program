573674d8a3000fc9c65ac5dca7f6e0646aa32067
Gleb Stashinsky <g.stashinsky@gmail.com>
2021-10-31 18:41:04 +0300

    fix: remove console.log

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -12,13 +12,11 @@ class UserController {
 
   getUsers(req: Request, res: Response): void {
     const { loginSubstring, limit } = req.query;
-    console.log('getUsers:', { loginSubstring }, { limit });
 
     const users = loginSubstring
       ? this.userService.getAutoSuggestUsers(String(loginSubstring), Number(limit))
       : this.userService.getAllUsers();
 
-    console.log('getAutoSuggestUsers:', { users });
     res.status(200).send(users);
   }
 
@@ -27,59 +25,47 @@ class UserController {
     const user = this.userService.getUserById(String(id));
 
     if (user) {
-      console.log('getUserById:', { user });
       return res.status(200).send(user);
     }
-    console.log('getUserById:', `user with id ${id} not found`);
     res.sendStatus(404);
   }
 
   createUser(req: Request, res: Response): Response {
     const user: User = req.body;
-    console.log('createUser:', { user });
 
     const { error } = UserSchema.validate(user);
     if (error) {
-      console.error('createUser:', error.message);
       return res.status(400).send(error.message);
     }
 
     const newUser = this.userService.createUser(user);
-    console.log('createUser:', 'user created', { newUser });
     res.status(201).send(newUser);
   }
 
   deleteUser(req: Request, res: Response): Response {
     const { id } = req.query;
-    console.log('deleteUser:', { id });
 
     if (this.userService.deleteUser(String(id))) {
-      console.log('deleteUser:', `user with id ${id} deleted`);
       return res.sendStatus(204);
     }
 
-    console.log('deleteUser:', `user with id ${id} not found`);
     res.sendStatus(404);
   }
 
   updateUser(req: Request, res: Response): Response {
     const fieldToUpdate: Partial<User> = req.body;
     const { id } = req.query;
-    console.log('updateUser:', { id }, { fieldToUpdate });
 
     const { error } = UserSchema.validate(fieldToUpdate);
     if (error) {
-      console.error('updateUser:', error.message);
       return res.status(400).send(error.message);
     }
 
     const user = this.userService.updateUser(String(id), fieldToUpdate);
     if (user) {
-      console.log('updateUser:', `user with id ${id} updated`, { user });
       return res.status(200).send(user);
     }
 
-    console.log('updateUser:', `user with id ${id} not found`);
     res.sendStatus(404);
   }
 }
